49f087b6fa2174fb2642bf33222d8bff6bae713d
laurolyra <laurolyra@gmail.com>
2019-12-13 19:00:55 -0300

    primeira animação da vassoura

--- Lauro/script.js
+++ Lauro/script.js
@@ -1,3 +1,9 @@
 function expandeArtigo () {
 	event.target.classList.toggle('cresce');
 }
+
+let vassoura = document.getElementById('vassoura');
+
+vassoura.addEventListener('click', function () {
+	vassoura.classList.toggle('anima-vassoura');
+})
