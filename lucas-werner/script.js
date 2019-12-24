e1b0b55dd18959f9b824bdd2620e7e6d736da3b7
Lucas-Werner <lucas.1werner@gmail.com>
2019-12-23 23:11:28 -0300

    criando pomo doidao

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,1 +1,18 @@
-asaa
+//pomo de ouro
+$(document).ready(function(){
+  animateDiv();
+});
+
+function makeNewPosition(){
+  var h = $(window).height() - 50;
+  var w = $(window).width() - 50;
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
+  return [nh,nw];
+}
+function animateDiv(){
+  var newq = makeNewPosition();
+  $('.pomo').animate({ top: newq[0], left: newq[1] }, function(){
+    animateDiv();
+  });
+};
