a634a441f380618486ae29e10053ea08c3c111c4
Lucas-Werner <lucas.1werner@gmail.com>
2019-12-25 12:01:52 -0300

    algumas correções

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,26 +1,25 @@
 // pomo de ouro
 let $ = ''
-$(document).ready(function(){
-  animateDiv();
-});
-
-function makeNewPosition(){
-  var h = $(window).height() - 60;
-  var w = $(window).width() - 60;
-  var nh = Math.floor(Math.random() * h);
-  var nw = Math.floor(Math.random() * w);
-  return [nh,nw];
-}
-function animateDiv(){
-  var newq = makeNewPosition();
-  $('.pomo').animate({ top: newq[0], left: newq[1] }, function(){
+function makeNewPosition() {
+  let h = $(window).height() - 60;
+  let w = $(window).width() - 60;
+  let nh = Math.floor(Math.random() * h);
+  let nw = Math.floor(Math.random() * w);
+  return [nh, nw];
+};
+function animateDiv() {
+  let newq = makeNewPosition();
+  $('.pomo').animate({ top: newq[0], left: newq[1] }, function () {
     animateDiv();
   });
 };
-$( ".pomo" ).click(function() {
+$('.pomo').click(function () {
   window.open('https://www.youtube.com/watch?v=inSjePhFe2o');
+})
+$(document).ready(function () {
+  animateDiv();
 });
-//vassoura voadora
+// vassoura voadora
 const nimbus = document.getElementById('nimbus2000');
 function voar() {
   nimbus.className = 'voa';
