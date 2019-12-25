049b39f411c74a954c5d2043f88926146c1f8052
Lucas-Werner <lucas.1werner@gmail.com>
2019-12-25 12:06:06 -0300

    code climate??

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,24 +1,24 @@
 // pomo de ouro
-let $ = ''
+const $ = ''; // "$" declarada pro Code Climate
 function makeNewPosition() {
-  let h = $(window).height() - 60;
-  let w = $(window).width() - 60;
-  let nh = Math.floor(Math.random() * h);
-  let nw = Math.floor(Math.random() * w);
+  const h = $(window).height() - 60;
+  const w = $(window).width() - 60;
+  const nh = Math.floor(Math.random() * h);
+  const nw = Math.floor(Math.random() * w);
   return [nh, nw];
-};
+}
 function animateDiv() {
-  let newq = makeNewPosition();
+  const newq = makeNewPosition();
   $('.pomo').animate({ top: newq[0], left: newq[1] }, function () {
     animateDiv();
   });
-};
+}
 $('.pomo').click(function () {
   window.open('https://www.youtube.com/watch?v=inSjePhFe2o');
-})
+});
 $(document).ready(function () {
   animateDiv();
-});
+})
 // vassoura voadora
 const nimbus = document.getElementById('nimbus2000');
 function voar() {
