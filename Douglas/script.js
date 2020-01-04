13fc4f71e4229f3c2cbab69aec7bc93d90e8a697
Douglas Henrique <doughrqe@gmail.com>
2020-01-04 12:51:00 -0300

    Code Climate

--- Douglas/script.js
+++ Douglas/script.js
@@ -2,8 +2,6 @@ const compra = document.getElementById('compra');
 const vassoura = document.querySelector('.vassoura');
 const novaMensagem = document.getElementById('fuga');
 const anuncio = document.getElementById('anuncio');
-compra.addEventListener('click', vaiVassoura );
-vassoura.addEventListener('click', vaiVassoura );
 function vaiVassoura () {
   vassoura.classList.add('voar');
   setTimeout(function(){
@@ -13,3 +11,6 @@ function vaiVassoura () {
 
   }, 6000)
 }
+compra.addEventListener('click', vaiVassoura );
+vassoura.addEventListener('click', vaiVassoura );
+
