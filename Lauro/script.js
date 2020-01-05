5f7d8b1977620d61c1255d9822c045b335223c09
Lauro Lyra Aguiar <50916624+laurolyra@users.noreply.github.com>
2020-01-05 19:15:29 -0300

    Novas adequações ao codeclimate.

--- Lauro/script.js
+++ Lauro/script.js
@@ -3,19 +3,19 @@ window.addEventListener("load", AnimaInicial);
 const vassoura = document.querySelector('.vassoura');
 
 function expandeArtigo () {
-    event.target.classList.add('cresce');
-    event.target.addEventListener('animationend', function () {
-        event.target.classList.remove('cresce');
-    })
+  event.target.classList.add('cresce');
+  event.target.addEventListener('animationend', function () {
+    event.target.classList.remove('cresce');
+  })
 }
 
 vassoura.addEventListener('click', function () {
-	vassoura.classList.toggle('anima-vassoura');
+  vassoura.classList.toggle('anima-vassoura');
 })
 
 function AnimaInicial() {
 	const pomo = document.getElementById('pomo');
 	setTimeout(function() {
-		pomo.classList.add('anima-pomo');
-	}, 3000);
+    pomo.classList.add('anima-pomo');
+}, 3000);
 }
