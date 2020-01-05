6d5e9eb2de27fd4ac7a1f28a4103bad335a1d5f7
Lauro Lyra Aguiar <50916624+laurolyra@users.noreply.github.com>
2020-01-05 05:28:38 -0300

    animações quase prontas.
    falta expandeartigo()

--- Lauro/script.js
+++ Lauro/script.js
@@ -1,9 +1,18 @@
+window.addEventListener("load", AnimaInicial);
+
+let vassoura = document.getElementById('vassoura2');
+
 function expandeArtigo () {
 	event.target.classList.toggle('cresce');
 }
 
-let vassoura = document.getElementById('vassoura');
-
 vassoura.addEventListener('click', function () {
 	vassoura.classList.toggle('anima-vassoura');
 })
+
+function AnimaInicial() {
+	const pomo = document.getElementById('pomo-img');
+	setTimeout(function() {
+		pomo.classList.add('animacao-inicial');
+	}, 3000);
+}
