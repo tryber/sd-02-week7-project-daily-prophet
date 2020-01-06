faaf7274d84049ec299a2e969a090d4a1231da7f
guicgs <etohlif@protonmail.com>
2020-01-06 00:10:07 -0300

    animação vassoura concluída

--- guicgs/profeta.js
+++ guicgs/profeta.js
@@ -1,7 +1,5 @@
 document.getElementById('anuncio-vassoura');
-
 vassoura.addEventListener('click', broom_animation);
-
 function broom_animation() {
   vassoura.classList.add('animacao-vassoura');
 }
