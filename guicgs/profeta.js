ba05f1f5ebb45ac429ca5569471de3e9f4bbe4ff
guicgs <etohlif@protonmail.com>
2020-01-05 22:42:53 -0300

    animação pássaro concluída

---
+++ guicgs/profeta.js
@@ -0,0 +1,7 @@
+document.getElementById('anuncio-vassoura');
+
+vassoura.addEventListener('click', broom_animation);
+
+function broom_animation() {
+  vassoura.classList.add('animacao-vassoura');
+}
