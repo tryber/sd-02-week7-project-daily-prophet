becdcfb3c4801b30e80f24ef5157f96c4218170c
guicgs <etohlif@protonmail.com>
2020-01-06 01:35:11 -0300

    primeira versão animação texto

--- guicgs/profeta.js
+++ guicgs/profeta.js
@@ -1,5 +1,20 @@
-document.getElementById('anuncio-vassoura');
-vassoura.addEventListener('click', broom_animation);
-function broom_animation() {
+const vassoura = document.getElementById('vassoura');
+vassoura.addEventListener('click', broomAnimation);
+function broomAnimation() {
   vassoura.classList.add('animacao-vassoura');
 }
+
+const textos = document.getElementsByClassName('texto-artigo');
+for (let texto of textos) {
+  texto.addEventListener('click', function() {
+    texto.style.transform = 'scale(1.5)';
+    texto.style.fontWeight = 'bold';
+  })
+}
+
+// const textoAnimado = document.getElementsByClassName('animacao-texto');
+// textoAnimado.addEventListener('change', animarTexto);
+// function animarTexto() {
+//   textoAnimado.style.width = "150%";
+//   textoAnimado.style.textDecoration = "bold";
+// }
