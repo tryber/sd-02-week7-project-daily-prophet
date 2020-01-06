786ce6f5c19bf4c9ed798dbca31da3306485227d
EddyeBoy27 <lipe0503@gmail.com>
2020-01-06 02:03:13 -0200

    Finalizado CodeClimate de JavaScript

--- Daily-Prophet/script.js
+++ Daily-Prophet/script.js
@@ -1,16 +1,16 @@
-const vassoura = document.getElementsByClassName('imagem-nimbus')[0]
-const aumenta1 = document.querySelectorAll('section')
+const vassoura = document.getElementsByClassName('imagem-nimbus')[0];
+const aumenta1 = document.querySelectorAll('section');
 
 vassoura.addEventListener('click', function voaNimbus() {
-  vassoura.classList.add('nimbus-voando')
-})
+  vassoura.classList.add('nimbus-voando');
+});
 
 function mudaClasse() {
-event.target.classList.toggle('aumentaTexto');
+  event.target.classList.toggle('aumentaTexto');
 }
 
 function clicaTexto(item) {
-item.addEventListener('click', mudaClasse);
+  item.addEventListener('click', mudaClasse);
 }
 
 aumenta1.forEach(clicaTexto);
