b5af241abe3b01c1b5113ce2b36730ae31dc2d24
guicgs <etohlif@protonmail.com>
2020-01-06 02:57:52 -0300

    algumas correções nos títulos e CC

--- guicgs/profeta.js
+++ guicgs/profeta.js
@@ -1,14 +1,14 @@
 const vassoura = document.getElementById('vassoura');
-vassoura.addEventListener ('click', function() {
+vassoura.addEventListener('click', function () {
   vassoura.classList.add('animacao-vassoura');
 });
 
 const textos = document.getElementsByClassName('texto-artigo');
 for (const texto of textos) {
-  texto.addEventListener ('click', function() {
+  texto.addEventListener('click', function () {
     texto.style.transform = 'scale(1.5)';
     texto.style.fontWeight = 'bold';
-    texto.addEventListener ('click', function() {
+    texto.addEventListener('click', function () {
       texto.style.transform = 'scale(1)';
       texto.style.fontWeight = 'normal';
     });
