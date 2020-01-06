53158b6d7583c3d84c1f563eeaf3abf375e88d7b
Giorgia <giorgialeoniac@gmail.com>
2020-01-06 00:33:59 -0300

    Inseridas alterações em HTML, CSS e JS. Terminado o JS com o crescimento das sections pelo click, criação das funções necessárias.

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -1,8 +1,20 @@
 const publi = document.querySelector('.ads');
 const broom = document.querySelector('.broom-img');
+const sectionArr = document.querySelectorAll('section');
 
-function createClass (){
-    broom.classList.add('broom-animation');
+function createClass() {
+  broom.classList.add('broom-animation');
+}
+publi.addEventListener('click', createClass);
+
+function growSection() {
+    let item = event.target;
+    item.classList.toggle('bigger-section');
 }
 
-publi.addEventListener ('click', createClass);
+function clickOn (item){
+    item.addEventListener('click', growSection);
+    console.log('rolou');
+}
+
+sectionArr.forEach(clickOn);
