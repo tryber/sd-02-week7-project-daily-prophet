64c3595e29b6eb85c5d07a359482b05287d8a00f
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-06 12:42:41 -0200

    code climate css e js

--- JulioCezar/script.js
+++ JulioCezar/script.js
@@ -1,16 +1,18 @@
+function crescerArtigo(event) {
+  const artigo = event.target;
+  artigo.classList.add('crescer_artigo');
+  artigo.style.transform = 'scale(1.5)';
+  artigo.style.fontWeight = 1000;
+}
+
+function animacaoVassoura(event) {
+  const vassouraMagica = event.target;
+  vassouraMagica.classList.add('vassoura');
+}
+
 const vassoura = document.querySelector('.img_vassoura');
 vassoura.addEventListener('click', animacaoVassoura);
 const artigos = document.querySelectorAll('.artigos');
 artigos.forEach((element) => {
   element.addEventListener('click', crescerArtigo);
 });
-
-function crescerArtigo(event) {
-  event.target.classList.add('crescer_artigo');
-  event.target.style.transform = 'scale(1.5)';
-  event.target.style.fontWeight = 1000;
-}
-
-function animacaoVassoura(event) {
-  event.target.classList.add('vassoura');
-}
