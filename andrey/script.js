c4abb461dd6b6aa418410a97efeb68dbe9c0a8cb
Andrey <andreytsuzuki@gmail.com>
2020-01-08 16:44:47 -0300

    segunda entrega

--- andrey/script.js
+++ andrey/script.js
@@ -13,3 +13,17 @@ const nimbus = document.querySelector('img.nimbus');
 nimbus.addEventListener('click', (e) => {
   e.target.style.animationPlayState = 'running';
 });
+
+const articles = document.querySelectorAll('article');
+const h2 = document.querySelectorAll('h2');
+const h1 = document.querySelectorAll('h1');
+
+const array = [].concat(...articles, ...h2, ...h1);
+
+array.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.target.style.animationPlayState = 'running';
+  });
+});
+
+
