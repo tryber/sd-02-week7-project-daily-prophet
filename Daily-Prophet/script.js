527edee376f7abc07632214c0e6e05ea9d33ff34
EddyeBoy27 <lipe0503@gmail.com>
2020-01-09 04:18:44 -0200

    Atualizado Projeto

--- Daily-Prophet/script.js
+++ Daily-Prophet/script.js
@@ -1,5 +1,8 @@
 const vassoura = document.getElementsByClassName('imagem-nimbus')[0];
 const aumenta1 = document.querySelectorAll('section');
+const pauseAnimation = document.querySelector('.cobrePegadas');
+const pegadas = document.querySelector('#pegadas');
+const severus = document.querySelector('.cover')
 
 vassoura.addEventListener('click', function voaNimbus() {
   vassoura.classList.add('nimbus-voando');
@@ -16,3 +19,17 @@ function eventoClick(event) {
 
 aumenta1.forEach(eventoClick);
 
+
+const pausaAnimacao = () => {
+  severus.style.animationPlayState = "paused"
+  pegadas.style.animationPlayState = "paused"
+  pauseAnimation.style.cursor = "pointer"};
+  
+  
+const rodaAnimacao = () => {
+  severus.style.animationPlayState = "running"
+  pegadas.style.animationPlayState = "running"};
+    
+pauseAnimation.addEventListener('mouseover', pausaAnimacao);
+pauseAnimation.addEventListener('mouseout', rodaAnimacao);
+    
